f5e1e4a30aebce4b2a49929e6cc4e2faaaf3dc90
jose <josemb125@gmail.com>
2017-09-04 06:19:21 -0500

    Reto4
    Cambiar la perspectiva de un objeto 2d

---
+++ Reto4/CasitaWireFrame3D_Vieja/CasitaWireFrame3D/src/Math/Projection.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Math;
+
+/**
+ *
+ * @author htrefftz
+ */
+public class Projection extends Matrix4x4 {
+    
+    public Projection() {
+        super();
+    }
+    
+    public Projection(double d) {
+        super();
+    }
+}
